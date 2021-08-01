7a13626e1e70b2023d625644e4dd32f70ce48cd7
Kuchuk Andrey <kreezag@gmail.com>
2021-08-01 16:05:27 +0400

    use dotenv config

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,8 +1,9 @@
 const axios = require('axios')
+require('dotenv').config()
 
 module.exports = async function () {
   try {
-    const result = await axios.get('https://newsapi.org/v2/top-headlines?country=us&sortBy=publishedAt&apiKey=a2c83e699b7b4b8380ad4daea6848e61')
+    const result = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&sortBy=publishedAt&apiKey=${process.env.API_KEY}&pageSize=5`)
     
     return result.data;
   } catch (e) {
