571dc50d03c7a7721ae7750f7298b6b686bf3073
Kuchuk Andrey <kreezag@gmail.com>
2021-08-01 16:10:49 +0400

    update apikey with netlify

--- src/_data/news.js
+++ src/_data/news.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 module.exports = async function () {
   try {
-    const result = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&sortBy=publishedAt&apiKey=${process.env.API_KEY}&pageSize=5`)
+    const result = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&sortBy=publishedAt&apiKey=${process.env.NEXS_API_KEY}&pageSize=5`)
     
     return result.data;
   } catch (e) {
