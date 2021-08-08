1cdfdbf0440ab232de15507c0257026146c9bfc9
Kuchuk Andrey <kreezag@gmail.com>
2021-08-08 12:42:42 +0400

    fix build error

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,29 +1,28 @@
-const axios = require('axios')
-const countries = require('./countries.json')
-require('dotenv').config()
 
-async function getCountryNews (country) {
+const axios = require("axios");
+const countries = require("./countries.json");
+
+require('dotenv').config();
+
+async function getNews(country) {
   try {
-    const result = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&sortBy=publishedAt&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`)
-    
-    return result.data || []
-  } catch (e) {
-    console.error(e)
+    const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
+    return {
+      "country": country,
+      "articles": response.data.articles
+    }
+  } catch (error) {
+    console.error(error);
   }
 }
 
-module.exports = async function () {
-  return getCountryNews(countries.join(',')).then((result = []) => {
+
+module.exports = async function() {
   
-    return Object.entries(
-      result.filter(Boolean).reduce((res, item) => ({
-        ...res,
-        [item.country]: [ ...res[item.country], item ]
-      }), {})
-    )
-      .reduce((res, [key, value]) => ({
-        country: key,
-        articles: value
-      }), [])
-  })
-}
+  var newsPromises = countries.map(getNews);
+  return Promise.all(newsPromises).then( newsObjects => {
+    console.log('newsObjects:', newsObjects);
+    return [].concat.apply([],newsObjects);
+  });
+  
+};
